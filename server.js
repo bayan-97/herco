748b64de1f645ce08d37371c60fefb3b4fc9a5e5
bayan97 <bayanalalem0@gmail.com>
2020-08-27 14:26:34 +0300

    hercoo

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 'use strict';
 
-// make a variable called express to use the express library
+
 const express = require('express');
 
-// initialize the express
-// server has lot of properties and methods
+
 const server = express(); 
 
-// declare a PORT to connect your website with the server
+
 const PORT = process.env.PORT ||1997;
 
-// to access files and folders inside public
+
 server.use(express.static('./public'));
 
 
-// http://localhost:3000/location?city=amman
-// http://localhost:3000/data
-server.get('/data',(request,response) =>{
-    response.send('You are doing great!!');
+
+server.get('/me',(request,response) =>{
+    response.send('Hi my name is bayan');
 })
 
-// http://localhost:3000/family
+
 server.get('/family',(req,res)=>{
     let familyArr = [
-        {name:'Atallah'},
-        {name: 'Mesina'},
-        {name:'Razan'}
+        {name:'Ahmad'},
+        {name: 'Omer'},
+        {name:'Afnan'}
     ];
     res.json(familyArr);
+    
+    
+   
 })
 
 
